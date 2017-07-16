7cac8f851b31a025b8ad2922caecd95ae28c67d6
esteb <4801459559Ee>
2017-07-16 03:21:53 -0400

    working on states

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,17 +10,68 @@ import Order from './Order';
 // Importing Inventory component
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
+import Fish from './Fish'
 
 class App extends React.Component {
+
+    constructor() {
+        // initialize the component
+        super();
+        // bind the method so we can use
+        this.addToOrder = this.addToOrder.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+        this.addFish = this.addFish.bind(this);
+        // sets the initial state
+        this.state = {
+            fishes: {},
+            order: {}
+        }
+    }
+
+    addToOrder(key) {
+        // This is the initial grab for state
+        const order = {...this.state.order};
+        // This adds the new fish
+        // logic is saying this order at this key is either going to be plus 1 or if there is none put 1
+        order[key] = order[key] +1 || 1;
+        // Update your state this is basically saying put this new order object into the state order
+        this.setState( { order: order } )
+
+    }
+
+    addFish(fish) {
+        // This will grab the initial state before change
+        const fishes = {...this.state.fishes};
+        // this will add the new fish
+        const timestamp = Date.now()
+        fishes[`fish-${timestamp}`] = fish;
+        // set it as state
+        this.setState({ fishes });
+    }
+
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
+
     render() {
         return(
             <div className="catch-of-the-day">
                 <div className="menu">
                     { /* this is passing in a prop with the key of 'tagline' and a value */}
                     <Header tagline="Fresh Seafood Market"/>
+                    <ul className="list-of-fishes">
+                        {Object
+                            .keys(this.state.fishes)
+                            .map(key => <Fish index={key} addToOrder={this.addToOrder} key={key} details={this.state.fishes[key]}/>)
+                        }
+                    </ul>
                 </div>
-                <Order />
-                <Inventory />
+                <Order fishes={this.state.fishes} order={this.state.order}/>
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
     }
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+class Fish extends React.Component {
+    render() {
+        // this is like saying this.props.details
+        const { details, index } = this.props;
+        const isAvailable = details.status === 'available';
+        // this const is saying if is available is true then text will show as 'Add To Order' if not it will show as 'Sold Out!;
+        const buttonText = isAvailable ? 'Add To Order' : 'Sold Out!';
+        return (
+            <li className="menu-fish">
+                <img src={details.image} alt={details.name}/>
+                <h3 className="fish-name">
+                    {details.name}
+                    <span className="price">{formatPrice(details.price)}</span>
+                </h3>
+                <p>{details.desc}</p>
+                <button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
+            </li>
+        )
+    }
+}
+
+export default Fish
